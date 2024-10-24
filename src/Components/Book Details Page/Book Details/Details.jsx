9d8b53866c621707865ae98ad89748bdb7d3de36
Mhamadalawieh <118356810+Mhamadalawieh@users.noreply.github.com>
2024-10-24 17:00:45 +0300

    commit

--- src/Components/Book Details Page/Book Details/Details.jsx
+++ src/Components/Book Details Page/Book Details/Details.jsx
@@ -377,7 +377,7 @@ const handleSuivreCategory = async () => {
                                     ).toFixed(2)
                               }`}{" "}
                               {bookData.discount > 0 && <span style={{opacity: "0.8",textDecoration:'line-through',fontSize: "calc(.9rem + 0.3vw)",margin:'0 1em'}} >
-                               {currency === "eur" ? `${Number(bookData._prix_public_ttc).toFixed(2)} € `: `${(bookData._prix_public_ttc * authCtx.currencyRate ).toFixed(2)} $ `}</span>}  
+                               {currency === "eur" ? `€${Number(bookData._prix_public_ttc).toFixed(2)} `: `$${(bookData._prix_public_ttc * authCtx.currencyRate ).toFixed(2)} `}</span>}  
                     
                      {bookData.discount > 0 && <span style={{background:'var(--primary-color)', color:'#fff', padding:'0.2em 0.8em',fontSize: "calc(.9rem + 0.3vw)",borderRadius:'5px'}} >
                       {bookData.discount}%</span>} 
