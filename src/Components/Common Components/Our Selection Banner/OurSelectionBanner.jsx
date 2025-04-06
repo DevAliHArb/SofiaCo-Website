bc60a7a157f22834ccb6ba8d1a72bc669637d239
Mhamadalawieh <dev.moe.a@gmail.com>
2025-04-06 15:02:27 +0300

    commit

--- src/Components/Common Components/Our Selection Banner/OurSelectionBanner.jsx
+++ src/Components/Common Components/Our Selection Banner/OurSelectionBanner.jsx
@@ -11,7 +11,11 @@ const OurSelectionBanner = ({props}) => {
   const language = useSelector(
     (state) => state.products.selectedLanguage[0].Language
   );
- 
+  const openInNewTab = (url) => {
+    if (url) {
+      window.open(url, "_blank", "noopener,noreferrer");
+    }
+  };
   return (
     <div className={classes.banner} onClick={()=>console.log(props)}>
       <div className={classes.content}>
@@ -21,7 +25,7 @@ const OurSelectionBanner = ({props}) => {
         <div className={classes.data}>
           <h1>{language === 'eng' ? props?.title_en : props?.title_fr }</h1>
           <p style={{width:'100%'}}>{language === 'eng' ? props?.subtitle_en : props?.subtitle_fr }</p>
-          <button onClick={()=>navigate(`/books`)} className={classes.buttoncart}>{data.OurSelectionBanner.button[language]}</button>
+          <button onClick={()=>openInNewTab(props?.button_url)} className={classes.buttoncart}>{language === 'eng' ? props?.button_label_en : props?.button_label_fr }</button>
         </div>
       </div>
     </div>
