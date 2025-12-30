aee053e649fede971776014180934d9a0b257c64
Mhamadalawieh <dev.moe.a@gmail.com>
2025-12-30 11:50:15 +0200

    pl-3288: security update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import securityHeadersPlugin from './src/api/securityHeadersPlugin.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), securityHeadersPlugin()],
 })
